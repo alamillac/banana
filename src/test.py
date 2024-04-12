c8488da764a4e00e30c70b5e75ce602251ea2f98
Andres Lamilla <aflamillac@gmail.com>
2024-04-12 10:33:12 +0200

    New visual model

--- src/test.py
+++ src/test.py
@@ -1,16 +1,16 @@
 from agent import Agent, DQN
 from environment import Env
 
-visual = True
+visual = False
 if visual:
     save_path = "checkpoint_visual.pth"
 else:
     save_path = "checkpoint.pth"
 
-env = Env(train_mode=True, visual=visual, seed=0)
+env = Env(train_mode=False, visual=visual, seed=0)
 
 dqn = DQN()
 agent = Agent(state_size=env.state_size, action_size=env.action_size, visual=visual, seed=0)
 agent.load(save_path)
 
-dqn.test(env, agent, num_episodes=100)
+dqn.test(env, agent, num_episodes=2)
