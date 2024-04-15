9f6e83b3ced7677311394e64bd55920933c714ff
Andres Lamilla <aflamillac@gmail.com>
2024-04-15 09:22:07 +0200

    Add train_until

--- src/agent/dqn.py
+++ src/agent/dqn.py
@@ -7,7 +7,7 @@ import numpy as np
 class DQN:
     def __init__(
         self,
-        n_episodes=2000,
+        max_episodes=2000,
         max_t=1000,
         eps_start=1.0,
         eps_end=0.01,
@@ -18,14 +18,14 @@ class DQN:
 
         Params
         ======
-            n_episodes (int): maximum number of training episodes
+            max_episodes (int): maximum number of training episodes
             max_t (int): maximum number of timesteps per episode
             eps_start (float): starting value of epsilon, for epsilon-greedy action selection
             eps_end (float): minimum value of epsilon
             eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
         """
         self.max_t = max_t
-        self.n_episodes = n_episodes
+        self.max_episodes = max_episodes
         self.eps_start = eps_start
         self.eps_end = eps_end
         self.eps_decay = eps_decay
@@ -65,12 +65,9 @@ class DQN:
         env.close()
         return scores
 
-    def train(self, env, agent):
-        scores = []  # list containing scores from each episode
-        scores_window = deque(maxlen=100)  # last 100 scores
+    def _train(self, env, agent, print_step=False):
         eps = self.eps_start  # initialize epsilon
-        max_score = -np.Inf
-        for i_episode in range(1, self.n_episodes + 1):
+        for i_episode in range(1, self.max_episodes + 1):
             state = env.reset()
             score = 0
             step = 0
@@ -80,13 +77,41 @@ class DQN:
                 agent.step(state, action, reward, next_state, done)
                 state = next_state
                 score += reward
-                print(f"\rStep {step} Action {action} Score: {score}", end="")
+                if print_step:
+                    print(f"\rStep {step} Action {action} Score: {score}", end="")
                 step += 1
                 if done:
                     break
-            scores_window.append(score)  # save most recent score
-            scores.append(score)  # save most recent score
             eps = max(self.eps_end, self.eps_decay * eps)  # decrease epsilon
+            yield i_episode, score
+
+    def train_until(self, env, agent, desired_score, consecutive_episodes=100):
+        scores_window = deque(maxlen=consecutive_episodes)  # last scores
+        scores = []  # list containing scores from each episode
+        for i_episode, score in self._train(env, agent):
+            scores.append(score)
+
+            scores_window.append(score)  # save most recent score
+            avg_score = np.mean(scores_window)
+
+            if avg_score >= desired_score and i_episode > consecutive_episodes:
+                print(f"\nEnvironment solved in {i_episode} episodes!\tAverage Score: {avg_score:.2f}")
+                break
+
+            print(f"\rEpisode {i_episode} Average Score: {avg_score:.2f}", end="")
+
+        agent.save(self.save_path)
+        env.close()
+        return scores
+
+    def train(self, env, agent):
+        scores_window = deque(maxlen=100)  # last 100 scores
+        scores = []  # list containing scores from each episode
+        max_score = -np.Inf
+        for i_episode, score in self._train(env, agent, print_step=True):
+            scores.append(score)
+
+            scores_window.append(score)  # save most recent score
             avg_score = np.mean(scores_window)
 
             # Save the model if the agent is improving
--- src/train.py
+++ src/train.py
@@ -15,8 +15,10 @@ print("Number of agents:", env.num_agents)
 print("Number of actions:", env.action_size)
 print("States have length:", env.state_size)
 
-dqn = DQN(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, save_path=save_path)
+dqn = DQN(max_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995, save_path=save_path)
+
 agent = Agent(state_size=env.state_size, action_size=env.action_size, visual=visual, seed=0)
 
+#scores = dqn.train_until(env, agent, desired_score=13, consecutive_episodes=100)
 scores = dqn.train(env, agent)
 dqn.plot_scores(scores)
